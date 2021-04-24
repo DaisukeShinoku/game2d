3792db3c1c12cbb42cb2dda640189d781b976939
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2021-04-24 14:24:10 +0900

    Moving sprites with physics

--- js/main.js
+++ js/main.js
@@ -13,6 +13,8 @@ PlayState.update = function() {
     this.hero.move(-1);
   } else if (this.keys.right.isDown) {
     this.hero.move(1);
+  } else {
+    this.hero.move(0);
   }
 };
 
@@ -32,13 +34,16 @@ PlayState.preload = function (){
 function Hero(game, x, y) {
   Phaser.Sprite.call(this, game, x, y, 'hero');
   this.anchor.set(0.5, 0.5);
+  this.game.physics.enable(this);
+  this.body.collideWorldBounds = true;
 }
 
 Hero.prototype = Object.create(Phaser.Sprite.prototype);
 Hero.prototype.constructor = Hero;
 
 Hero.prototype.move = function(direction) {
-  this.x += direction * 2.5;
+  const SPEED = 200;
+  this.body.velocity.x = direction * SPEED;
 }
 
 // create game entities & set up world
